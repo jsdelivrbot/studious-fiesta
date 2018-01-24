1fc8681455e936b868c4666c81beace66b2f8465
Shurui Liu <shurui91@gmail.com>
2018-01-23 17:03:28 -0800

    started to work on class level compotents

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDpC9Cq4zOng4oq1mfkh_WXgFtHEXUUA9I';
 
 const App = () => {
-	return <div> Hi! </div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
