b2d993ef648ab0bc142b828309db1b33d9e6809d
Shurui Liu <shurui91@gmail.com>
2018-01-29 00:40:04 -0800

    making things to be React classes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDpC9Cq4zOng4oq1mfkh_WXgFtHEXUUA9I';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-	console.log(data);
-});
-
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+			this.setState({ videos });
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
