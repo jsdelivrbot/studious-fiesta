6b5b7442bfe93c00c4346666b5f695719f44c004
Shurui Liu <shurui91@gmail.com>
2018-01-28 21:04:03 -0800

    start to communicate with YT APIs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDpC9Cq4zOng4oq1mfkh_WXgFtHEXUUA9I';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+	console.log(data);
+});
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
