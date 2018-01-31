b8c7e7d14eedfee925a7ee519caa059fea9ef512
Shurui Liu <shurui91@gmail.com>
2018-01-30 16:03:13 -0800

    fixed file naming error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDpC9Cq4zOng4oq1mfkh_WXgFtHEXUUA9I';
 
